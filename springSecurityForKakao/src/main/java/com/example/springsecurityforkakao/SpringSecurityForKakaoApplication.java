1d780df37bc4efd8fe1f5a4d6d41d9e62d56cb38
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-05-27 17:26:26 +0900

    feature: project create

---
+++ springSecurityForKakao/src/main/java/com/example/springsecurityforkakao/SpringSecurityForKakaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityforkakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityForKakaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityForKakaoApplication.class, args);
+    }
+
+}
